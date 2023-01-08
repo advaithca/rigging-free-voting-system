b2d845cdca7109cf3f0af4547916eeeb9bdef102
Aaryashivani <sundropdiamond@gmail.com>
2023-01-08 10:57:04 +0530

    Manually copied embedder file from main branch and added integrations

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify
 import os 
 from routes.voter import voter_info_api
-import datetime
-
-x = datetime.datetime.now()
 
 PORT = 5000
 FOLDER_NAME = "backend"
@@ -16,17 +13,6 @@ app.register_blueprint(voter_info_api, url_prefix="/voter")
 def index():
     print(request)
     return jsonify(success=True)
-
-@app.route('/data')
-def get_time():
-  
-    # Returning an api for showing in  reactjs
-    return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":x, 
-        "programming":"python"
-        }
 
 if __name__ == '__main__':
     app.run(port=PORT, debug=True)
