f0234cb4440abcdc6bdee24e95fc6b324f3595df
Aron-droid <aronc.anand@gmail.com>
2023-02-08 13:02:07 +0530

    push by aron

---
+++ voting-system-main/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
