d65b12cbc47f2a26e2504090507e9046b2cd1696
Aaryashivani <sundropdiamond@gmail.com>
2023-03-12 17:06:04 +0530

    Added script to create pickle file and made changes to FaceRecognizer to 1. change embedding data to numpy array to prevent value error on internal conversion to numpy array 2. reduces no of components to 50 as training set size reduced to 52 after deleting unnecessary documents from MongoDB

--- backend/FaceRecognizer.py
+++ backend/FaceRecognizer.py
@@ -4,6 +4,7 @@ from sklearn.decomposition import PCA as RandomizedPCA
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import numpy as np 
 
 class FaceRecognizer():
     def __init__(self,data):
@@ -14,17 +15,19 @@ class FaceRecognizer():
         '''
         self.faceData = pd.DataFrame(list(data.find()))
 
-        self.X = self.faceData['embeddings']
-        self.y = self.faceData['labels']
+        embedding =  self.faceData['embedding']
+        embedding_data = np.array([np.array(x) for x in embedding])
+        self.X = embedding_data                  
+        self.y = self.faceData['label']
+
     
     def make_model(self):
-        pca = RandomizedPCA(n_components=150, whiten=True, random_state=42)
+        pca = RandomizedPCA(n_components=50, whiten=True, random_state=42)
         svc = SVC(kernel='rbf', class_weight='balanced')
         self.model = make_pipeline(pca, svc)
     
     def train(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.4, random_state=42)
-        
         self.model.fit(self.X_train, self.y_train)
 
     def test(self):
--- backend/scripts/add.py
+++ backend/scripts/add.py
@@ -11,9 +11,10 @@ with open(os.path.join(os.getcwd(), "jsontest.json")) as f:
     json_data = json.load(f)
     #print(json_data)
     for jsonObjDict in json_data:
-        jsonObjDict["_id"]["oid"] = jsonObjDict["_id"]["$oid"]
-        del jsonObjDict["_id"]["$oid"]
-        print(jsonObjDict)
+        jsonObjDict["_id"]["oid"] = jsonObjDict["_id"].pop("$oid")
+        jsonObjDict["label"] = jsonObjDict.pop("name")
+        jsonObjDict["embedding"] = jsonObjDict.pop("encoding")
+        #print(jsonObjDict)
         requesting.append(InsertOne(jsonObjDict))
 
 result = collection.bulk_write(requesting)
