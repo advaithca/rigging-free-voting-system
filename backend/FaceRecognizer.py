0abf95d675c40f790f808097b768cd1c792d8a2e
advaithca <advaith29042002@gmail.com>
2023-03-13 15:45:58 +0530

    Update FaceRecognizer.py

--- backend/FaceRecognizer.py
+++ backend/FaceRecognizer.py
@@ -1,51 +1,52 @@
 import pandas as pd
+from pymongo import MongoClient
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from sklearn.decomposition import PCA as RandomizedPCA
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import numpy as np 
-import pymongo as pm
 
-class FaceRecognizer():
-    def __init__(self,data):
-        '''
-        Initialise the Object with face data
+class FaceRecognizer:
+    def __init__(self, collection):
+        data = []
+        for record in collection.find():
+            data.append(record)
+        df = pd.DataFrame(data)
+        for i in range(128):
+            df[i] = 0
+        for row in df.itertuples():
+            for i in range(len(row[3])):
+                df.loc[row[0],i] = row[3][i]
+        df.drop(['_id', 'embedding'], axis=1, inplace=True)
 
-        data: Pymongo document cursor
-        '''
-        self.faceData = pd.DataFrame(list(data.find()))
+        self.X = df.drop('label', axis=1)
+        self.y = df['label']
 
-        embedding =  self.faceData['embedding']
-        embedding_data = np.array([np.array(x) for x in embedding])
-        self.X = embedding_data                  
-        self.y = self.faceData['label']
-
-    
     def make_model(self):
-        pca = RandomizedPCA(n_components=40, whiten=True, random_state=42)
-        svc = SVC(kernel='rbf', class_weight='balanced')
-        self.model = make_pipeline(pca, svc)
-    
+        pipeline = Pipeline([
+            ('pca', PCA(n_components=32)),
+            ('svm', SVC())
+        ])
+        self.model = pipeline
+
     def train(self):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.4, random_state=42)
-        self.model.fit(self.X_train, self.y_train)
+        X_train, self.X_test, y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
 
     def test(self):
-        predictions = self.model.predict(self.X_test)
-        self.score = accuracy_score(predictions, self.y_test)
+        accuracy = self.model.score(self.X_test, self.y_test)
+        self.score = accuracy
     
     def update(self, x, y):
         self.model.fit(x, y)
 
     def get_prediction(self, data):
         return self.model.predict(data) 
-    
+
 def validateVoter(name:str):
     '''
     Returns whether a voter of given name is Valid or Not
     '''
-    connection = pm.MongoClient(
+    connection = MongoClient(
         "mongodb+srv://majorproject:majorproject@cluster0.ktbjam0.mongodb.net/?retryWrites=true&w=majority"
     )
     db = connection['voterDetails']
