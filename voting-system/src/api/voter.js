54506e6faf6ceaaed7ae5b597e08c45bb7e316e4
advaithca <advaith29042002@gmail.com>
2023-03-23 11:50:35 +0530

    Update voter.js

--- voting-system/src/api/voter.js
+++ voting-system/src/api/voter.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://172.17.1.43:5000"
+const BASE_URL = "https://childlike-birds-production.up.railway.app"
 const IMAGE_UPLOAD_URL_SUFFIX = "/voter/upload/voterDetails";
 const TRAIN_MODEL_URL_SUFFIX = "/voter/train";
 const VOTER_SCAN_URL_SUFFIX = "/voter/imageProcess";
