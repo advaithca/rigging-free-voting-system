8861bd5f67cb56722820bdf7da88ca1bec63e229
advaithca <advaith29042002@gmail.com>
2023-03-23 09:41:42 +0530

    Update voter.js

--- voting-system/src/api/voter.js
+++ voting-system/src/api/voter.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "http://172.17.0.87:5000"
 const IMAGE_UPLOAD_URL_SUFFIX = "/voter/upload/voterDetails";
 const TRAIN_MODEL_URL_SUFFIX = "/voter/train";
 const VOTER_SCAN_URL_SUFFIX = "/voter/imageProcess";
